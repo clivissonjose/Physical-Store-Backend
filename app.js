fbd03f228ba03421b4553658727cddb86e38f104
clivissonjose <cjbarbosa2001@gmail.com>
2024-10-31 14:38:18 -0300

    feat: refatorando projeto

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const app =  express();
-const logger = require("./utils/logger");
+const app = express();
+const logger = require("./src/utils/logger");
 
-logger.info('Iniciando o aplicativo...');
+logger.info("Iniciando o aplicativo...");
 app.use(express.json());
 // app.use(express.static(`${__dirname}/public`));
 
-const lojaRouter = require("./routes/lojasRoutes");
+const lojaRouter = require("./src/routes/lojasRoutes");
 
-app.use("/api/v1/lojas", lojaRouter); 
+app.use("/api/v1/lojas", lojaRouter);
 
 module.exports = app;
--- src/controllers/LojaController.js
+++ src/controllers/LojaController.js
@@ -1,101 +1,95 @@
-const Loja = require("./../model/lojaModel");
+const Loja = require("../model/lojaModel");
 const buscarEndereco = require("../services/buscarEnderecoService");
 const geoLocalizacao = require("../services/buscarCoordenadasService");
 const calcularDistancia = require("../services/calcularDistanciaService");
+const logger = require("../utils/logger");
 //import calcularDistancia from "../services/calcularDistanciaService.js";
 
-exports.pegarLojas = async(req, res)  => {  
-    try {
+exports.pegarLojas = async (req, res) => {
+  try {
+    const lojas = await Loja.find();
 
-      const lojas = await Loja.find();
-  
-      console.log(lojas);
-      res.status(200).json({
-        status: 'success',
-        results: lojas.length,
-        data: {
-          lojas
-        }
-      });
-    } catch (err) {
-      res.status(500).json({
-        status: 'error',
-        message: 'Algo deu errado!',
-      });
-    }
-  };
+    logger.info("Lojas recuperadas com sucesso", { total: lojas.length });
+    // console.log(lojas);
+    res.status(200).json({
+      status: "success",
+      results: lojas.length,
+      data: {
+        lojas,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Algo deu errado!",
+    });
+  }
+};
 //async function pegarMesmoCep(req, res) {
 exports.pegarMesmoCep = async (req, res) => {
-
-  try{
+  try {
     console.log("CEP buscado:", req.params.cep);
-    const lojas =  await Loja.find({ cep: req.params.cep });
-  
-    if(!lojas){
+    const lojas = await Loja.find({ cep: req.params.cep });
+
+    if (!lojas) {
       return res.status(404).json({
         status: "Fail",
         message: "Não foi possível achar tal cep!",
-      })
-  }
- // console.log("Quantidade de lojas: ", lojas.length); -> undefined  
+      });
+    }
+    // console.log("Quantidade de lojas: ", lojas.length); -> undefined
     res.status(200).json({
       status: "Success9",
       data: {
-        lojas: lojas
-      }
+        lojas: lojas,
+      },
     });
-  }catch(err){
+  } catch (err) {
     res.status(500).json({
-       status: "Error",
-       message: err.message
-    })
+      status: "Error",
+      message: err.message,
+    });
   }
-}
+};
 
-exports.deletarLoja = async (req,res) => {
-  try{
-
-  const loja =  await Loja.findOneAndDelete({ cep: req.params.cep });
-
-  if(!loja){
-    console.log("Não foi possível  deletar loja!");
-  }
-
-  res.status.json({
-    status: "success",
-    data:{
-      loja
-    }
-  });
-
-  }catch(err){
-    
-  }
-}
-exports.criarLoja = async (req, res) => {
-
+exports.deletarLoja = async (req, res) => {
   try {
+    const loja = await Loja.findOneAndDelete({ cep: req.params.cep });
 
+    if (!loja) {
+      console.log("Não foi possível  deletar loja!");
+    }
+
+    res.status.json({
+      status: "success",
+      data: {
+        loja,
+      },
+    });
+  } catch (err) {}
+};
+exports.criarLoja = async (req, res) => {
+  try {
     // Pegar cep digitado pelo usuário
     const cep = await buscarEndereco(req.body.cep);
 
     console.log(cep);
     // Constante para pegar latitude e longitude
     const latLong = await geoLocalizacao(cep.cep);
-   
+
     const latitude = latLong.latitude;
     const longitude = latLong.longitude;
 
     // Adicionar loja no Bando de Dados
     const novaLoja = await Loja.create({
-      nome: "Magazine Luiza",         
-      cep:cep.cep,
+      nome: "Magazine Luiza",
+      cep: cep.cep,
       logradouro: cep.logradouro,
       complemento: cep.complemento,
       latitude: latitude,
       longitude: longitude,
       unidade: cep.unidade,
-      bairro:   cep.bairro,
+      bairro: cep.bairro,
       localidade: cep.cidade,
       uf: cep.uf,
       estado: cep.estado,
@@ -103,23 +97,22 @@ exports.criarLoja = async (req, res) => {
       ibge: cep.ibge,
       gia: cep.gia,
       ddd: cep.ddd,
-      siafi: cep.siafi
+      siafi: cep.siafi,
     });
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       data: {
-        loja: novaLoja
-      }
+        loja: novaLoja,
+      },
     });
   } catch (err) {
     res.status(400).json({
-      status: 'fail',
-      message: err.message
+      status: "fail",
+      message: err.message,
     });
   }
 };
-
 
 exports.lojasProximas100km = async (req, res) => {
   try {
@@ -127,12 +120,17 @@ exports.lojasProximas100km = async (req, res) => {
     const lojas = await Loja.find();
 
     // Filtra as lojas no raio de 100km e adiciona a distância calculada
-    
+
     const lojasProximas = lojas
       .map((el) => {
-        const distancia = calcularDistancia(latLong.latitude, latLong.longitude, el.latitude, el.longitude);
+        const distancia = calcularDistancia(
+          latLong.latitude,
+          latLong.longitude,
+          el.latitude,
+          el.longitude
+        );
         return { ...el._doc, distancia }; // usa o _doc para copiar os dados da loja e adicionar a distância
-     })
+      })
       .filter((el) => el.distancia <= 100);
 
     // Ordena as lojasProximas pela distância
